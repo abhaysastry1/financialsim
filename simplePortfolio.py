e081361b8743830a29c855411edbb9dfcda4aa7a
Michael Tilaka <mdt53229@uga.edu>
2021-05-21 13:19:36 -0400

    expenditures, income, random

--- simplePortfolio.py
+++ simplePortfolio.py
@@ -1,15 +1,28 @@
+import random
+
 # Test Variables
 pCash = 100000
 spend = 100
 retStart = 2021
 retEnd = 3350
-
+income = 410000
 
 # Function for calculation
-def portfolio(port, exp, start, end):
+def portfolio(port, exp, start, end, income):
     for x in range(start, end):
         port -= exp
+        print("Port is (  exp change  ): " + str(round(port, 2)) + " in " + str(x))
+
+        port += income
+        print ("Port is (income change): " + str(round(port, 2)) + " in " + str(x))
+
+        port -= random.uniform(0.0, (0.33) * exp)
+        print ("Port is (random change): " + str(round(port, 2)) + " in " + str(x) + "\n")
+
     if port < 0:
         return 'Not enough cash to retire'
     else:
         return 'You have ' + str(port) + ' remaining after death'
+
+#Some num in between 0-1/3 exp 
+#Some port -= num
--- userInput.py
+++ userInput.py
@@ -1,8 +1,9 @@
-from portfolio.simplePortfolio import portfolio
+from simplePortfolio import portfolio
 
 start = input("When does retirement start?")
 end = input("When does retirement end?")
 assets = input("How much fricking money do you have?")
+income = input ("How much do u make per year?")
 spending = input("How much do you spend?")
 
-print(portfolio(int(assets), int(spending), start, end))
+print(portfolio(int(assets), int(spending), int(start), int(end), int(income)))
