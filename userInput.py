4e91dc90ee41eaa8e0edf8f562546ad2428fecd7
Rohbiar <73039664+Rohbiar@users.noreply.github.com>
2021-05-24 18:07:52 -0400

    Delete userInput.py

--- userInput.py
+++
@@ -1,9 +0,0 @@
-from simplePortfolio import portfolio
-
-start = input("When does retirement start?")
-end = input("When does retirement end?")
-assets = input("How much fricking money do you have?")
-income = input ("How much do u make per year?")
-spending = input("How much do you spend?")
-
-print(portfolio(int(assets), int(spending), int(start), int(end), int(income)))
